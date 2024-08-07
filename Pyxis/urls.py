c6b26a1df3943bec97eb622610c7c7e5cf4f62f9
arr <ukujude1@gmail.com>
2024-08-07 12:48:22 +0300

    widget dialogue support

--- Pyxis/urls.py
+++ Pyxis/urls.py
@@ -7,7 +7,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path('official_kkn_bot/', admin.site.urls),
     path('', include('chat.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
