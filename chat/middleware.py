1467d9d78fbeef71a4e06e998ca68b5fbb4f9d28
arr <ukujude1@gmail.com>
2026-01-16 21:51:47 +0300

    added link ddna6.pro to middleware.py

--- chat/middleware.py
+++ chat/middleware.py
@@ -496,7 +496,7 @@ class ContentSecurityPolicyMiddleware:
             "https://ddna.site",
             "https://ddna1.info",
             "https://ddna1.pro",
-            "https://ddna2.info",
+            "https://ddna6.pro",
             "https://ddna2.pro",
             "https://ddna3.pro",
             "https://ddna3.info",
